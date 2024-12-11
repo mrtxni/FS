d612f22468c8839d475d5cbff9875bf8d9d4737d
mrtxni <akunikshannn@gmail.com>
2024-12-12 01:38:39 +0700

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b> Hello {first}</b>\n\n<b>Untuk dapat melihat video kamu harus join channel dahulu, Untuk join vip tanpa link hub @noturamel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b> Hello {first}</b>\n\n<b>Untuk dapat melihat video kamu harus join channel dahulu, Untuk join vip tanpa link hub @cumasiska</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
